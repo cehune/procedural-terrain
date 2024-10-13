87ead549d2c2d12f00d1525cb179baf55458aedb
mcka <mckale.chung@gmail.com>
2024-10-13 17:59:31 -0400

    Added camera utils + made it 3D

--- procedural-terrain/Source.cpp
+++ procedural-terrain/Source.cpp
@@ -6,18 +6,32 @@
 #include "./utils/perlin.h"
 #include "./utils/texture.h"
 #include "./utils/shader.h"
+#include "./utils/camera.h"
 #include <math.h>
 #include <vector> // Make sure to include vector
 #include "SFML/Graphics.hpp"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
-//void renderHeightmap(GLuint heightmapTextureID);
 
 // settings
 int Perlin::p[Perlin::GRADIENT_COUNT * 2];
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+const unsigned int NUM_STRIPS = 400 - 1;
+const unsigned int NUM_VERTS_PER_STRIP = 400 * 2;
+
+Camera camera(glm::vec3(0.0f, 19.0f, 59.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -41,7 +55,12 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     // load all openGL function pointers with glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -49,9 +68,11 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     Shader shader("vertex.vs", "fragment.fs");
 
-    float quadVertices[] = {
+    /*float quadVertices[] = {
         // positions     // texCoords
         -0.3f,  0.3f, 0.0f,  0.0f, 1.0f, // top-left
         -0.3f, -0.3f, 0.0f,  0.0f, 0.0f, // bottom-left
@@ -62,7 +83,11 @@ int main()
     unsigned int quadIndices[] = {
         0, 1, 2,
         0, 2, 3
-    };
+    };*/
+
+    std::vector<float> textureData = perlin.generateHeightMap(400, 400, 400);
+    unsigned int heightmapID = Texture().generate2DArray(textureData, 400, 400);
+    std::vector<unsigned int> indices = perlin.generateHeightMapIndices(400, 400);
 
     unsigned int VBO, VAO, EBO;
 
@@ -72,40 +97,53 @@ int main()
     glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, textureData.size() * sizeof(float), textureData.data(), GL_STATIC_DRAW);
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    std::vector<float> textureData = perlin.generateHeightMap(400, 400, 400);
-    unsigned int heightmapID = Texture().generate2DArray(textureData, 400, 400);
-    
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
 
     // render loop
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         // input
         processInput(window);
 
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        shader.use();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);        shader.use();
 
         // Render the heightmap
-        glActiveTexture(GL_TEXTURE0); // Activate the texture unit 0
-        glBindTexture(GL_TEXTURE_2D, heightmapID); // Bind your heightmap texture
+        //glActiveTexture(GL_TEXTURE0); // Activate the texture unit 0
+        //glBindTexture(GL_TEXTURE_2D, heightmapID); // Bind your heightmap texture
 
         // Set the sampler2D uniform to point to texture unit 0 in your fragment shader
-        shader.setInt("heightmapTexture", 0);
+        //shader.setInt("heightmapTexture", 0);
         // Bind the VAO of your quad
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        shader.glUniformMat4("projection", projection);
+        shader.glUniformMat4("view", view);
+        shader.glUniformMat4("model", model);
+        glBindVertexArray(VAO);
+        // render the mesh triangle strip by triangle strip - each row at a time
+        for (unsigned int strip = 0; strip < NUM_STRIPS; ++strip)
+        {
+            glDrawElements(GL_TRIANGLE_STRIP,   // primitive type
+                NUM_VERTS_PER_STRIP, // number of indices to render
+                GL_UNSIGNED_INT,     // index data type
+                (void*)(sizeof(unsigned int)
+                    * NUM_VERTS_PER_STRIP
+                    * strip)); // offset to starting index
+        }
         glfwSwapBuffers(window);
         
         glfwPollEvents();
@@ -119,16 +157,59 @@ int main()
     glfwTerminate();
     return 0;
 }
-
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
 
 
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
--- procedural-terrain/fragment.fs
+++ procedural-terrain/fragment.fs
@@ -1,14 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;  // Make sure you're passing texture coordinates
+//in vec2 TexCoords;  // Make sure you're passing texture coordinates
 
-uniform sampler2D heightmapTexture;  // Your heightmap texture
+//uniform sampler2D heightmapTexture;  // Your heightmap texture
 
 void main() {
     // Sample the heightmap texture using the passed texture coordinates
-    float heightValue = texture(heightmapTexture, TexCoords).r;
+    //float heightValue = texture(heightmapTexture, TexCoords).r;
     
     // Render as grayscale or adjust based on heightValue
-    FragColor = vec4(vec3(heightValue), 1.0);
+    FragColor = vec4(0.4, 0.3, 0.1, 1.0);
 }
